fdec858a97fb817f97fd831617e4ae2c1cb18a09
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2014-10-09 15:44:12 +0200

    Added basic local storage integration to projects

--- app/scripts/mainModule.js
+++ app/scripts/mainModule.js
@@ -1,2 +1,2 @@
-angular.module('plannrs', [])
+angular.module('plannrs', ['LocalStorageModule'])
   .constant('API_URL', 'http://localhost:8080/api/proxy')
--- app/scripts/mainService.js
+++ app/scripts/mainService.js
@@ -1,13 +1,29 @@
 angular.module('plannrs')
-  .service('mainService', ['$http', '$q', 'API_URL', function($http, $q, API_URL) {
+  .service('mainService', ['$http', '$q', 'localStorageService', 'API_URL', function($http, $q, localStorageService, API_URL) {
     var self = this;
+
+    var _isPreviouslyStored = function(key, defer) {
+      var storedValue = localStorageService.get(key)
+      if(storedValue) {
+        defer.resolve(storedValue);
+        return true;
+      } else {
+        return false
+      }
+    }
+
     self.getProjects = function() {
-      var defer = $q.defer();
-      $http({method: 'GET', url: API_URL, params:{url:'/project'}}).success(function(response) {
-        return defer.resolve(response);
-      }).error(function(error) {
-        return defer.reject(error);
-      })
+      var key = 'GET_PROJECTS',
+          defer = $q.defer();
+
+      if(!_isPreviouslyStored(key, defer)) {
+        $http({method: 'GET', url: API_URL, params:{url:'/project'}}).success(function(response) {
+          localStorageService.set(key, response);
+          return defer.resolve(response);
+        }).error(function(error) {
+          return defer.reject(error);
+        });
+      }
       return defer.promise;
     }
 
